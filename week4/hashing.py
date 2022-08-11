9c31d72bc9f4d32feb3beea412c43d0f4af39425
carlosrobinson <carlos.robinson@bryanuniversity.edu>
2022-08-11 17:34:12 -0400

    week4

--- week4/hashing.py
+++ week4/hashing.py
@@ -4,6 +4,10 @@
 #         self.value = value 
         
         
+from audioop import mul
+from posixpath import split
+
+
 class HashTable:
     def __init__(self) -> None:
         self.size = 10
@@ -13,44 +17,48 @@ class HashTable:
 
     def hashfunction(self,key):
         # Insert your hashing function code
-        mult = 1
+        hash_key = str(key)
         hv = 0
-        for ch in [key]:
-            hv += mult * ch
-            mult += 1
-        return hv % self.size 
+        for ch in hash_key:
+            hv +=  ord(ch)
+        return (hv * len(hash_key))  % self.size
+        
 
     def rehash(self,key):
         # Insert your secondary hashing function code
-        mult = 1
-        hv = 0
-        for ch in [key]:
-            hv += mult * ch
-            mult += 1
-        return hv // self.size + 1
+        return key // self.size 
 
     def put(self,key,data):
         # Insert your code here to store key and data 
+        items = [key, data]
         h = self.hashfunction(key)
-        while self.slots[h] is not None: 
-            if self.slots[h] is key: 
-                break 
-            h = (h + 1) % self.size 
-        self.slots[h] = key
-        self.data[h] = data
-            
-            
+        if self.slots[h] is None:
+            self.slots[h] = items[0]
+            self.data[h] = items[1]
+        else:
+            if self.slots[h] is key:
+              self.data[h] = items[1]  
+            else:
+                h = self.rehash(key)
+                if self.slots[h] is None:
+                    self.slots[h] = items[0]
+                    self.data[h] = items[1]
+                else:
+                    return None
             
         
  
     def get(self,key):
         # Insert your code here to get data by key
         h = self.hashfunction(key)
-        if self.slots[h] is not None: 
-            if self.slots[h] is key: 
-                return self.data[h]
-            h = (h+ 1) % self.size
-        return None
+        if self.slots[h] is key: 
+            return self.data[h]
+        else:
+            h = self.rehash(key)
+        if(self.slots[h] == key):
+            return self.data[h]
+        else:
+            return None
 
     def __getitem__ (self,key):
         return self.get(key)
@@ -70,6 +78,7 @@ H[52] = 'F'
 H[89] = 'G'
 H[70] = 'H'
 H[12] = 'I'
+
 
 # print the slot values
 print(H.slots)
